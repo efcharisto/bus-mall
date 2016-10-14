392b61cd804641f47f96b09b69595fd7b01b0eba
Maksim Mikoss <pvl1720@gmail.com>
2016-10-13 20:57:28 -0700

    fixed the unexpected token error for dot at the bottom when calling method

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 'use strict';
 
-// function Photo(name, path, vote) {
-//   this.name = name;
-//   this.path = path;
-//   this.vote = 0;
-//   this.totalVotes = 0;
-// }
+function Photo(name, path) {
+  this.name = name;
+  this.path = path;
+  this.vote = 0;
+  this.totalVotes = 0;
+  this.pixArray = ['img/bag.jpg', 'img/pen.jpg', 'img/usb.jpg'];
+};
 
 var photoTest = {
   name: name,
-  path: path,
+  href: 'img/bag.jpg',
   vote: 0,
   totalVotes: 0,
+  pixArray: ['img/bag.jpg', 'img/pen.jpg', 'img/usb.jpg'],
 
   randomNum: function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -22,10 +24,11 @@ var photoTest = {
   // },
 
   showPics: function(){
-    for (var i=0; i < 2; i++){
-      console.log("Hello!");
+    for (var i = 0; i < 2; i++){
+      console.log('Hello!');
     }//testing
   },
-},
+};
 
 photoTest.showPics();
+// Photo();
