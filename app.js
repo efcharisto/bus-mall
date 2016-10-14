0938ebef24239c802d02c2e12138de8d8ea27d17
Maksim Mikoss <pvl1720@gmail.com>
2016-10-13 20:31:01 -0700

    something is wrong

--- app.js
+++ app.js
@@ -7,27 +7,25 @@
 //   this.totalVotes = 0;
 // }
 
-var photos = {
-  name : name,
-  path : path,
-  vote : 0,
+var photoTest = {
+  name: name,
+  path: path,
+  vote: 0,
   totalVotes: 0,
 
   randomNum: function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+  },
+  //random number generator method
+  // randomNum: function(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1) + min);
+  // },
 
   showPics: function(){
     for (var i=0; i < 2; i++){
       console.log("Hello!");
     }//testing
   },
+},
 
-}
-
-
-  //random number generator method
-  // randomNum: function(min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1) + min);
-  // },
-  photo.showPic();
+photoTest.showPics();
