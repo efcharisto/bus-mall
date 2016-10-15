1d6728adeb3a416571c7a30730d6e8cad3fd941b
Maksim Mikoss <pvl1720@gmail.com>
2016-10-15 15:06:45 -0700

    testing pushing element to DOM. now I successfully used js to insert a pic into html document and it works

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var showAllPix = new Array();
 
-function Photo(name, path) {
+function Photo(name, path, vote) {
   this.name = name;
   this.location = path;
   this.vote = 0;
@@ -14,17 +14,26 @@ function Photo(name, path) {
   //   return Math.floor(Math.random() * (max - min + 1) + min);
   // },
 
-  showPics: function() { //unexpected token (  what?..
-    for (var i = 0; i < 2; i++){
-      console.log('Hello!');
-    }
-  },
+  //   showPics: function() { //unexpected token (  what?..
+  //     for (var i = 0; i < 2; i++){
+  //       console.log('Hello!');
+  //     }
+  //   },
 };
+
+//trying to display 1 img using DOM.
+//got help here
+// http://stackoverflow.com/questions/2735881/adding-images-to-the-html-with-javascript
+function domStuff() {
+  var img = document.createElement('img');
+  img.src = 'http://www.boxmaker.com/wp-content/uploads/2015/12/seattle-icon.png';
+  var src = document.getElementById('products');
+  src.appendChild(img);
+}
+
+domStuff();
 
 //random number generator method
 // randomNum: function(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min);
 // },
-
-var pic = new Photo('bag', 'img/bag.jpg');
-// Photo();
