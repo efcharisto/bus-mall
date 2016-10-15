826fa8a582aa68a315b19b145f00232bbb803f91
Maksim Mikoss <pvl1720@gmail.com>
2016-10-15 14:17:31 -0700

    working on constructor and still dont know why I have a parsing error at line 17

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 'use strict';
 
+var showAllPix = new Array();
+
 function Photo(name, path) {
   this.name = name;
-  this.path = path;
+  this.location = path;
   this.vote = 0;
   this.totalVotes = 0;
-  this.pixArray = ['img/bag.jpg', 'img/pen.jpg', 'img/usb.jpg'];
-};
 
-var photoTest = {
-  name: name,
-  href: 'img/bag.jpg',
-  vote: 0,
-  totalVotes: 0,
-  pixArray: ['img/bag.jpg', 'img/pen.jpg', 'img/usb.jpg'],
+  var pixArray = new Array('img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg');
 
-  randomNum: function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  //random number generator method
   // randomNum: function(min, max) {
   //   return Math.floor(Math.random() * (max - min + 1) + min);
   // },
 
-  showPics: function(){
+  showPics: function() { //unexpected token (  what?..
     for (var i = 0; i < 2; i++){
       console.log('Hello!');
-    }//testing
+    }
   },
 };
 
-photoTest.showPics();
+//random number generator method
+// randomNum: function(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// },
+
+var pic = new Photo('bag', 'img/bag.jpg');
 // Photo();
