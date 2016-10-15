779408836262ca26b58c6fe8c2a51ef193486e5c
Maksim Mikoss <pvl1720@gmail.com>
2016-10-15 16:13:48 -0700

    minor tweaks in html

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 'use strict';
 
-var showAllPix = new Array();
-
-function Photo(name, path, vote) {
+function Photo(name, path) {
   this.name = name;
   this.location = path;
   this.vote = 0;
   this.totalVotes = 0;
 
-  var pixArray = new Array('img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg');
+  var showAllPix = new Array();
+  var pixPath = new Array('img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg');
+
+  for (var i = 0; i <= pixPath.length; i++) {
+    this.push(showAllPix);
+  }
 
   // randomNum: function(min, max) {
   //   return Math.floor(Math.random() * (max - min + 1) + min);
   // },
-
-  //   showPics: function() { //unexpected token (  what?..
-  //     for (var i = 0; i < 2; i++){
-  //       console.log('Hello!');
-  //     }
-  //   },
 };
 
-//trying to display 1 img using DOM.
-//got help here
+//trying to display 1 img using DOM. Got help here
 // http://stackoverflow.com/questions/2735881/adding-images-to-the-html-with-javascript
 function domStuff() {
   var img = document.createElement('img');
   img.src = 'img/bag.jpg';
-  var src = document.getElementById('products');
+  var src = document.getElementById('product1');
   src.appendChild(img);
 }
 
