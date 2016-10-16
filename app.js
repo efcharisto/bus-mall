44d6f462dfdda42153f0f0e46f4fcde63eaf206b
Maksim Mikoss <pvl1720@gmail.com>
2016-10-15 18:01:11 -0700

    trying to randomize pix

--- app.js
+++ app.js
@@ -6,35 +6,34 @@ function Sale(name, path) {
   this.location = path;
   this.vote = 0;
   this.totalVotes = 0;
-  this.showAllPix = new Array();
-  this.pixPath = new Array('img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg');
 
+  this.showAllPix = new Array();
+  var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg'];
+
+  function randomProduct() {
+    for (var i = 0; i <= pixPath.length; i++) {
+      var randomPic = Math.floor((Math.random() * pixPath.length));;
+      //  this.showAllPix.push(path);
+      document.getElementById('product1').src = pixPath[randomPic];
+    }
+  }
+  randomProduct();
   // randomNum: function(min, max) {
   //   return Math.floor(Math.random() * (max - min + 1) + min);
   // },
 
-  pushNames: function() {
-    for (var i = 0; i <= this.pixPath.length; i++) {
-
-      var path = this.pixPath[i]
-      return Math.floor(Math.random() * (3 - 0 + 1) + min);
-      this.showAllPix.push(path);
-    }
-  },
-
   // for (var i = 0; i <= pixPath.length; i++) {
   //   this.push(showAllPix); } not working yet
-
 };
 
 //trying to display 1 img using DOM. Got help here
 // http://stackoverflow.com/questions/2735881/adding-images-to-the-html-with-javascript
+
 function domStuff() {
   var img = document.createElement('img');
   img.src = 'img/bag.jpg';
   var src = document.getElementById('product1');
   src.appendChild(img);
 }
-
-//var firstSale = new Sale('banana', 'img/banana.jpg');
+new Sale('bag', 'img/bag.jpg');
 domStuff();
