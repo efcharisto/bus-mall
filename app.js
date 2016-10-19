3a1a7ee672f937e94b16301dcd04c8eeea5558ef
Maksim Mikoss <pvl1720@gmail.com>
2016-10-18 19:44:45 -0700

    the struggle

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 'use strict';
 
+var showAllPix = [];
 
-function Sale(name, path) {
+var Sale = function(name, path) {
   this.name = name;
-  this.location = path;
+  this.path = path;
   this.vote = 0;
   this.totalVotes = 0;
-
-  this.showAllPix = new Array();
-  var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg'];
-
-  function randomProduct() {
-    var img = document.createElement('img');
-    img.src = pixPath[randomPic];
-    var src = document.getElementById('product1');
-    src.appendChild(img);
-    for (var i = 0; i <= pixPath.length; i++) {
-      var randomPic = Math.floor((Math.random() * pixPath.length));;
-      //  this.showAllPix.push(path);
-      document.getElementById('product1').src = pixPath[randomPic];
-    }
-  }
-  randomProduct();
-  // randomNum: function(min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1) + min);
-  // },
-
-  // for (var i = 0; i <= pixPath.length; i++) {
-  //   this.push(showAllPix); } not working yet
+  showAllPix.push(this);
 };
 
+function randomProduct() {
+  var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg'];
+  var img = document.createElement('img');
+  img.src = pixPath[i];
+  var src = document.getElementById('product1');
+  src.appendChild(img);
+  for (var i = 0; i <= pixPath.length; i++) {
+    var randomPic = Math.floor((Math.random() * pixPath.length));
+    //round a random value taken from array several times(arr length)
+    //this.showAllPix.push(path);
+    document.getElementById('product1').src = pixPath[randomPic];
+  }
+}
+
+randomProduct();
+// randomNum: function(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// },
+
+// for (var i = 0; i <= pixPath.length; i++) {
+//   this.push(showAllPix); } not working yet
+
 new Sale('bag', 'img/bag.jpg');
+new Sale('banana', 'img/banana.jpg');
+new Sale('bathroom', 'img/bathroom.jpg');
