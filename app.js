f74ff4401d071f70f7229494df98c7ab560a58f5
Maksim Mikoss <pvl1720@gmail.com>
2016-10-20 20:43:08 -0700

    got a single random image to show up

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var showAllPix = [];
 
-// var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg'];
+// var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg'];
 
 var Sale = function(name, pixPath) {
   this.name = name;
@@ -14,9 +14,10 @@ var Sale = function(name, pixPath) {
 
 function randomProduct() {
   // for (var i = 0; i < showAllPix.length; i++) {
-    var randomNum = Math.floor(Math.random() * showAllPix.length);
-    var product1 = document.getElementById('product1');
-    product1.src = showAllPix[randomNum].pixPath;
+  var randomNum = Math.floor(Math.random() * showAllPix.length);
+  //get 3 different random numbers first and then focus on the rest
+  var product1 = document.getElementById('product1');
+  product1.src = showAllPix[randomNum].pixPath;
 
   // }
 }
@@ -24,6 +25,13 @@ function randomProduct() {
 new Sale('bag', 'img/bag.jpg'); //not working yet, to be used later
 new Sale('banana', 'img/banana.jpg');
 new Sale('bathroom', 'img/bathroom.jpg');
+new Sale('boots', 'img/boots.jpg');
+new Sale('breakfast', 'img/breakfast.jpg');
+new Sale('bubblegum', 'img/bubblegum.jpg');
+new Sale('cthulhu', 'img/cthulhu.jpg');
+new Sale('chair', 'img/chair.jpg');
+new Sale('dog-duck', 'img/dog-duck.jpg');
+new Sale('dragon', 'img/dragon.jpg');
 randomProduct();
 
 // randomNum: function(min, max) {
