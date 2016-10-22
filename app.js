998a61c0feed257df15d407f1bb145138d760d50
Maksim Mikoss <pvl1720@gmail.com>
2016-10-22 12:37:49 -0700

    3 unique random images now display on the page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var showAllPix = [];
 
-// var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg'];
+//var pixPath = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg'];
 
 var Sale = function(name, pixPath) {
   this.name = name;
@@ -12,7 +12,7 @@ var Sale = function(name, pixPath) {
   showAllPix.push(this);
 };
 
-new Sale('bag', 'img/bag.jpg'); //not working yet, to be used later
+new Sale('bag', 'img/bag.jpg'); //manual call for each instance
 new Sale('banana', 'img/banana.jpg');
 new Sale('bathroom', 'img/bathroom.jpg');
 new Sale('boots', 'img/boots.jpg');
@@ -26,25 +26,23 @@ new Sale('dragon', 'img/dragon.jpg');
 function randomNum(){
   return Math.floor(Math.random() * showAllPix.length);
 }
-
+//random numbers to be assigned to each image
 var numberOne = randomNum();
 var numberTwo = randomNum();
 var numberThree = randomNum();
-//get 3 different random numbers first and then focus on the rest
+//making sure that all 3 random images are different
 function randomizer(){
-  //var randomNum = Math.floor(Math.random() * showAllPix.length);
   while (numberTwo === numberOne){
     numberTwo = randomNum();
-    console.log(numberTwo + ' is num 2 and ' + numberOne + ' is num 1.');
   }
-
   while (numberThree === numberTwo || numberThree === numberOne){
     numberThree = randomNum();
-    console.log(numberThree + ' is num 3 and ' + numberTwo + ' is num 2.');
   }
 }
-randomizer();
+randomizer(); // ^
 
+//DOM: gets each product by element to set source to
+//unique random values from all pix array
 function randomProducts() {
   var product1 = document.getElementById('product1');
   product1.src = showAllPix[numberOne].pixPath;
@@ -54,33 +52,5 @@ function randomProducts() {
 
   var product3 = document.getElementById('product3');
   product3.src = showAllPix[numberThree].pixPath;
-  // }
 }
-
-// function randomProduct2() {
-//   // for (var i = 0; i < showAllPix.length; i++) {
-//   var randomNum = Math.floor(Math.random() * showAllPix.length);
-//   //get 3 different random numbers first and then focus on the rest
-//   var product2 = document.getElementById('product2');
-//   product2.src = showAllPix[randomNum].pixPath;
-//   // }
-// }
-//
-// function randomProduct3() {
-//   // for (var i = 0; i < showAllPix.length; i++) {
-//   var randomNum = Math.floor(Math.random() * showAllPix.length);
-//   //get 3 different random numbers first and then focus on the rest
-//   var product3 = document.getElementById('product3');
-//   product3.src = showAllPix[randomNum].pixPath;
-//   // }
-// }
-
-
 randomProducts();
-
-// randomNum: function(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// },
-
-// for (var i = 0; i <= pixPath.length; i++) {
-//   this.push(showAllPix); } not working yet
